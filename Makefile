135881d4ec152054608e09b67d2805becec66b89
Christopher Biscardi <chris@christopherbiscardi.com>
2020-12-22 14:14:57 -0800

    maybe?

---
+++ Makefile
@@ -0,0 +1,31 @@
+.PHONY: build
+
+CRATES = $(dir $(wildcard ./crates/*/))
+
+help: ## Show this help.
+	@awk 'BEGIN {FS = ":.*?## "} /^[a-zA-Z_-]+:.*?## / {sub("\\\\n",sprintf("\n%22c"," "), $$2);printf "\033[36m%-20s\033[0m %s\n", $$1, $$2}' $(MAKEFILE_LIST)
+
+build:
+	@echo $(CRATES)
+	for crate in $(CRATES) ; do \
+		cargo install --path $$crate --root bins --target x86_64-unknown-linux-musl ;\
+	done
+	@mkdir -p functions
+	@cp bins/bin/* functions/
+
+create-guild-command:  ## Create a guild-scoped command. Guild commands update instantly and should be used for testing
+	@curl -XPOST \
+	  -H "Authorization: Bot $DISCORD_BOT_TOKEN" \
+	  -d @./crates/repl/options.json \
+	  https://discord.com/api/v8/applications/714618235458289804/guilds/601625579166367755/commands 
+	# For authorization, you can use either your bot token 
+	# headers = {
+	# 	"Authorization": "Bot 123456"
+	# }
+
+	# # or a client credentials token for your app with the applications.commmands.update scope
+	# headers = {
+	# 	"Authorization": "Bearer abcdefg"
+	# }
+create-global-command:  ## Create a global command, global commands are cached for 1h and should be used for "making public" the command
+	@echo "not implemented yet"
---
+++ crates/discord_interactions/src/lib.rs
@@ -0,0 +1,173 @@
+use http::HeaderMap;
+// use lazy_static::lazy_static;
+use netlify_lambda_http::Body;
+
+use serde::{Deserialize, Serialize};
+use serde_json::json;
+use serde_repr::{Deserialize_repr, Serialize_repr};
+use sodiumoxide::crypto::sign;
+
+/// Verify the discord signature using your application's publickey,
+/// the `X-Signature-Ed25519` and `X-Signature-Timestamp` headers,
+/// and the request body.
+///
+/// This is required because discord will send you a ping when
+/// you set up your webhook URL, as well as random invalid input
+/// periodically that has to be rejected.
+pub fn validate_discord_signature(
+    headers: &HeaderMap,
+    body: &Body,
+    pub_key: &sign::PublicKey,
+) -> bool {
+    let sig_ed25519 = headers
+        .get("X-Signature-Ed25519")
+        .map(|sig| sign::Signature::from_slice(sig.as_bytes()))
+        .flatten();
+    let sig_timestamp = headers.get("X-Signature-Timestamp");
+
+    if let (Body::Text(body), Some(timestamp), Some(sig_bytes)) = (body, sig_timestamp, sig_ed25519)
+    {
+        let content = timestamp
+            .as_bytes()
+            .into_iter()
+            .chain(body.as_bytes().into_iter())
+            .cloned()
+            .collect::<Vec<u8>>();
+
+        sign::verify_detached(&sig_bytes, &content.as_slice(), pub_key)
+    } else {
+        false
+    }
+}
+
+// snowflake
+#[derive(Debug, Serialize, Deserialize, Eq, PartialEq)]
+#[serde(tag = "type")]
+pub struct DiscordEvent<T> {
+    #[serde(rename = "type")]
+    pub event_type: EventType,
+    pub data: Option<T>,
+    pub guild_id: Snowflake,
+    pub channel_id: Snowflake,
+    pub member: GuildMember,
+    pub token: String,
+    // version: usize,
+}
+
+#[derive(Serialize_repr, Deserialize_repr, PartialEq, Eq, Debug)]
+#[repr(u8)]
+pub enum EventType {
+    Ping = 1,
+    ApplicationCommand = 2,
+}
+type Snowflake = String;
+#[derive(Debug, Serialize, Deserialize, Eq, PartialEq)]
+pub struct GuildMember {}
+#[derive(Debug, Serialize, Deserialize, Eq, PartialEq)]
+pub struct ApplicationCommandInteractionData(serde_json::Value);
+
+// struct ApplicationCommandInteractionDataOption {
+//     name: String,
+//     value: Option,
+//     options: Option,
+// }
+
+#[derive(Serialize, Deserialize, PartialEq, Eq, Debug)]
+pub struct InteractionResponse {
+    ir_type: InteractionResponseType,
+    data: Option<InteractionApplicationCommandCallbackData>,
+}
+#[derive(Serialize_repr, Deserialize_repr, PartialEq, Eq, Debug)]
+#[repr(u8)]
+pub enum InteractionResponseType {
+    Pong = 1,
+    Acknowledge = 2,
+    ChannelMessage = 3,
+    ChannelMessageWithSource = 4,
+    ACKWithSource = 5,
+}
+
+pub fn reply_with(ir_type: InteractionResponseType, content: String) -> InteractionResponse {
+    InteractionResponse {
+        ir_type,
+        data: Some(InteractionApplicationCommandCallbackData {
+            tts: false,
+            content,
+        }),
+    }
+}
+#[derive(Serialize, Deserialize, PartialEq, Eq, Debug)]
+pub struct InteractionApplicationCommandCallbackData {
+    tts: bool,
+    content: String,
+    // embeds: Option<Vec<Embed>>,                   // TODO,
+    // allowed_mentions: Option<Vec<AllowedMention>>, // TODO
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use serde_json;
+
+    #[test]
+    fn it_works() {
+        let parsed: DiscordEvent<serde_json::Value> = serde_json::from_str(
+            r#"{
+            "type": 2,
+            "token": "A_UNIQUE_TOKEN",
+            "member": {
+                "user": {
+                    "id": 53908232506183680,
+                    "username": "Mason",
+                    "avatar": "a_d5efa99b3eeaa7dd43acca82f5692432",
+                    "discriminator": "1337",
+                    "public_flags": 131141
+                },
+                "roles": ["539082325061836999"],
+                "premium_since": null,
+                "permissions": "2147483647",
+                "pending": false,
+                "nick": null,
+                "mute": false,
+                "joined_at": "2017-03-13T19:19:14.040000+00:00",
+                "is_pending": false,
+                "deaf": false
+            },
+            "id": "786008729715212338",
+            "guild_id": "290926798626357999",
+            "data": {
+                "options": [{
+                    "name": "cardname",
+                    "value": "The Gitrog Monster"
+                }],
+                "name": "cardsearch",
+                "id": "771825006014889984"
+            },
+            "channel_id": "645027906669510667"
+        }"#,
+        )
+        .unwrap();
+        assert_eq!(
+            parsed,
+            DiscordEvent {
+                event_type: EventType::ApplicationCommand,
+                data: serde_json::from_str(
+                    r#"{
+                    "options": [{
+                        "name": "cardname",
+                        "value": "The Gitrog Monster"
+                    }],
+                    "name": "cardsearch",
+                    "id": "771825006014889984"
+                }"#
+                )
+                .ok(),
+                guild_id: "290926798626357999".to_string(),
+                channel_id: "645027906669510667".to_string(),
+                member: GuildMember {},
+                token: "A_UNIQUE_TOKEN".to_string(),
+                // version: 1,
+            }
+        );
+    }
+}
